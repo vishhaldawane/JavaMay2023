4a72f703a0fa32bda158f089d82346cdfcea77e6
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-05-19 17:08:56 +0530

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,24 @@
+package com.bags;
+
+public class Song 
+{
+	private String title;
+	private String artist;
+	private String album;
+	private int year;
+	
+	
+	public Song(String title, String artist, String album, int year) {
+		super();
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	@Override
+	public String toString() {
+		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", year=" + year + "]";
+	}
+	
+	
+}
